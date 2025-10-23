679e08326b1d755e963eb82773309e9db82cd98a
EmaCst <dcastellanosv2@miumg.edu.gt>
2025-10-22 20:14:54 -0600

    pruebas con stripe

--- app/routes/factura.routes.js
+++ app/routes/factura.routes.js
@@ -1,7 +1,7 @@
 // routes/factura.routes.js
 const express = require("express");
 const router = express.Router();
-const facturaController = require("../controllers/factura.controller"); // 👈 corregido (minúscula)
+const facturaController = require("../controllers/Factura.controller"); // 👈 corregido (minúscula)
 
 // Crear una factura (con Stripe y Envío)
 router.post("/", facturaController.create);
