15fc10d836b739f8755881c043d68cf9de1a977a
EmaCst <dcastellanosv2@miumg.edu.gt>
2025-11-01 02:39:46 -0600

    dominio

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 // ==========================
 const allowedOrigins = [
   "http://localhost:5173", // desarrollo local
-  "https://proyecto-analisis-2.onrender.com", // dominio backend
+  "https://proyecto-analisis-2.onrender.com", 
    "https://zona404shoes.vercel.app"
 ];
 
